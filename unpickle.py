2ba9c8d776d3c85f90f71a179eedc0451fa434c1
Jim <morgan@ischool.berkeley.edu>
2013-10-09 11:07:37 -0700

    updated unpickle

--- unpickle.py
+++ unpickle.py
@@ -1,4 +1,7 @@
 import pickle
 
-parsed_reviews = pickle.load( open( "norton.p", "rb" ) )
-print parsed_reviews
+parsed_reviews = pickle.load( open( "MicroMP3.p", "rb" ) )
+
+print 'here is an example (the first item in the dictionary of "parsed_reviews":'
+print parsed_reviews.items()
+
