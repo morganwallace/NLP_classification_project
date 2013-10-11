ddc8eb883823271488504a651fb10158ab761698
Sonali <sonali.sharma@ischool.berkeley.edu>
2013-10-11 01:48:00 -0700

    added regular expression to check for positive, negative or neutral expressions and tagged the sentences accordingly

--- parse_training.py
+++ parse_training.py
@@ -2,6 +2,7 @@ import os
 import nltk
 import pickle
 import random
+import re
 
 def parse_reviews(directory,filename):
     f=open(directory+filename)
@@ -18,23 +19,30 @@ def parse_reviews(directory,filename):
 
             #mark the separation of attributes and sentences
             hashIndex=line.find('##')
-            attrObj={}
+            
             #if there are attributes then parse them this way
             if hashIndex>1:            
                 sent=line[hashIndex+2:]
                 attrString=line[:hashIndex]
                 #Converts the string of attributes to list
-                attrs=attrString.split(",")
-                for attr in attrs:
-                    attrObj[attr[:attr.find("[")]]=attr[attr.find("[")+1:-1]
+                #attrs=attrString.split(",")
+                m = re.findall('\[([+-])\d\]',attrString)
+                if len(set(m)) ==  1:
+                    if m[0] == "+": 
+                        tag = "pos"
+                    else:
+                        tag = "neg"
+                else:
+                    tag = "neutral"
+                #    attrObj[attr[:attr.find("[")]]=attr[attr.find("[")+1:-1]
                 
             #if there are no attributes, parse them this way           
             else:
                 sent=line[2:]
-                attrObj=None
+                #attrObj=None
                 
             #add to dictionary that keeps track of parsed sentences attributes and values
-            parsed_sents[sent]=attrObj
+            parsed_sents[sent]=tag
             
     pickledName=filename[:filename.rfind(".")]+".p"
     pickle.dump(parsed_sents,open(pickledName,"wb"))
