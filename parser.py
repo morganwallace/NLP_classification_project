cad83e81cc4745443ff6cefb82ef363fbc3f0223
Jim <morgan@ischool.berkeley.edu>
2013-10-14 11:51:38 -0700

    cleaned up the master repo. All .p files in sub dirs. removed my extraneous files from earlier.

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ def parse_reviews(directory, filename):
     f = open(trainingDir + filename)
     lines=f.readlines()
     f.close()
-    parsed_sents=[]
+    parsed_sents={}
     for line in lines:
         if line!="[t]\n":
                 #remove the '\r\n' from the end of each line
@@ -37,14 +37,17 @@ def parse_reviews(directory, filename):
 
                 parsed_sents[sent]=tag
         
+
         pickledName=filename[:filename.rfind(".")]+".p"
-        pickle.dump(parsed_sents,open(pickledName,"wb"))
+        pickle.dump(parsed_sents,open("heldout/"+pickledName,"wb"))
                 
 
-trainingDir = "../product_data_training_heldout/training/"
+trainingDir = "../product_data_training_heldout/heldout/"
 os.listdir(trainingDir)
 
-for trainingFile in os.listdir(trainingDir):
-    if trainingFile != ".DS_Store":
-        print trainingFile
-        parse_reviews(trainingDir,trainingFile)
+if __name__ == '__main__':
+    for File in os.listdir(trainingDir):
+        if File != ".DS_Store":
+            print File
+            parse_reviews(trainingDir,File)
+
